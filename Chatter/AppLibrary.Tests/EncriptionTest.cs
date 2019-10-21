4bf1e223c308b355d09385214f467f4ababed5b5
andrius <andri@DESKTOP-62HQ01O>
2019-10-21 22:31:13 +0300

    same test

---
+++ Chatter/AppLibrary.Tests/EncriptionTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using App;
+
+namespace AppLibrary.Tests
+{
+
+    public class EncriptionTest
+    {
+
+        [Theory]
+        [ InlineData ("bandymas")]
+        [InlineData("KitasBandymas652")]
+        public void Encrypt_ShouldChechIfEncriptionAndDecriptionWork(string bandymas)
+        {
+            string bandymasEncypted = Encryptor.Encrypt(bandymas);
+
+            Assert.Equal(Encryptor.Encrypt(bandymas), bandymasEncypted);
+            Assert.Equal(Encryptor.Decrypt(bandymasEncypted), bandymas);
+            Assert.Equal(Encryptor.Decrypt(bandymasEncypted), Encryptor.Decrypt(bandymasEncypted));
+
+        }
+
+
+    }
+}
---
+++ Chatter/AppLibrary.Tests/RegistrationTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.App;
+using Xunit;
+
+namespace AppLibrary.Tests
+{
+    public class RegistrationTests
+    {
+        private RegistrationValidator registrationValidator = new RegistrationValidator();
+
+        [Fact]
+        public void IsPasswordMatch_ShouldChechIfDataIsCurrect()
+        {
+
+            string message;
+
+            string password ="Slaptazodis",passwordRepeat =  "Slaptazodis";
+            
+
+             registrationValidator.IsPasswordMatch(password, passwordRepeat, out message);
+
+            Assert.Matches("password ok", message);
+        }
+        [Fact]
+        public void IsPasswordValid_ShouldCheckIfPasswordIsGood()
+        {
+
+            string message;
+
+            string password = "Slaptazodis";
+
+
+            registrationValidator.IsPasswordValid(password, out message);
+
+            Assert.Matches("password ok", message);
+        }
+        [Fact]
+        public void IsPasswordValid_ShouldCheckIfPasswordIsNull()
+        {
+
+            string message;
+
+            string password = null;
+
+
+            registrationValidator.IsPasswordValid(password, out message);
+
+            Assert.Matches("cannot be empty", message);
+        }
+
+        [Fact]
+        public void IsPasswordValid_ShouldCheckIfPasswordIsLong()
+        {
+
+            string message;
+
+            string password = "short";
+
+
+            registrationValidator.IsPasswordValid(password, out message);
+
+            Assert.Matches("6 chars min", message);
+        }
+
+        [Fact]
+        public void IsUserNameValid_ShouldCheckIfNameIsNotNull()
+        {
+
+            string message;
+
+            string Name = null;
+
+
+            registrationValidator.IsUserNameValid(Name, out message);
+
+            Assert.Matches("cannot be empty", message);
+        }
+
+        [Fact]
+        public void IsUserNameValid_ShouldCheckIfNameIsLong()
+        {
+
+            string message;
+
+            string name = "short";
+
+
+            registrationValidator.IsUserNameValid(name, out message);
+
+            Assert.Matches("6 chars min", message);
+        }
+
+    }
+}
