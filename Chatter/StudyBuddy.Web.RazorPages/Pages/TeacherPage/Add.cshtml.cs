82f4567dc56c438efae75b95fd97e6cbb8f380a2
Vaidas <vaidasmac04@gmail.com>
2019-11-04 14:10:39 +0200

    Unnecessary files deleted, add/delete subject

---
+++ Chatter/StudyBuddy.Web.RazorPages/Pages/TeacherPage/Add.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StudyBuddy.Web.RazorPages.Data;
+using StudyBuddy.Web.RazorPages.Models;
+
+namespace StudyBuddy.Web.RazorPages.Pages.TeacherPage
+{
+    public class AddModel : PageModel
+    {
+        private readonly StudyBuddy.Web.RazorPages.Data.StudiBudiContext _context;
+
+        [BindProperty]
+        public int SubjectID { get; set; }
+        public List<SelectListItem> Options { get; set; }
+
+        public AddModel(StudyBuddy.Web.RazorPages.Data.StudiBudiContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Options = await _context.Subject.Select(s =>
+                                                 new SelectListItem
+                                                 {
+                                                     Value = s.Subjectid.ToString(),
+                                                     Text = s.Title
+                                                 }).ToListAsync();
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int teacherID)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            if(!_context.Teaching.Any(t => t.Userid == teacherID && t.Subjectid == SubjectID))
+            {
+                _context.Teaching.Add(new Teaching() { Userid = teacherID, Subjectid = SubjectID });
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Subjects");
+            }
+
+            return Content("Subject already exist!");
+        }
+    }
+}
--- Chatter/StudyBuddy.Web.RazorPages/Pages/TeacherPage/Subjects.cshtml.cs
+++ Chatter/StudyBuddy.Web.RazorPages/Pages/TeacherPage/Subjects.cshtml.cs
@@ -21,8 +21,8 @@ namespace StudyBuddy.Web.RazorPages.Pages.TeacherPage
         }
 
         //todo: teacher id is hardcoded but it should be received after user logs in
-        private int teacherID = 3;
-        public List<string> MySubjects;
+        public int TeacherID = 3;
+        public List<string> MySubjects = new List<string>();
 
         public async Task OnGetAsync()
         {
@@ -31,8 +31,17 @@ namespace StudyBuddy.Web.RazorPages.Pages.TeacherPage
 
             MySubjects = (from t in teachings
                          join s in subjects on t.Subjectid equals s.Subjectid
-                         where t.Userid == teacherID
-                         select s.Title).ToList();
+                         where t.Userid == TeacherID
+                          select s.Title).ToList();
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(string subjectTitle)
+        {
+            int subjectID = await _context.Subject.Where(s => s.Title.Equals(subjectTitle)).Select(s => s.Subjectid).FirstOrDefaultAsync();
+            Teaching teaching = await _context.Teaching.Where(t => t.Userid == TeacherID && t.Subjectid == subjectID).FirstOrDefaultAsync();
+            _context.Remove(teaching);
+            await _context.SaveChangesAsync();
+            return Redirect("/TeacherPage/Subjects");
         }
     }
 }
