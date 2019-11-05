c3c6dd2eadeb60439cb6d59b6d9fd7b01b5449a1
Vaidas <vaidasmac04@gmail.com>
2019-11-05 11:36:56 +0200

    Empty chat and profile pages added for teacher

---
+++ Chatter/StudyBuddy.Web.RazorPages/Pages/TeacherPage/TeacherChat.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StudyBuddy.Web.RazorPages.Data;
+using StudyBuddy.Web.RazorPages.Models;
+
+namespace StudyBuddy.Web.RazorPages.Pages.TeacherPage
+{
+    public class TeacherChatModel : PageModel
+    {
+        private readonly StudyBuddy.Web.RazorPages.Data.StudiBudiContext _context;
+
+        public TeacherChatModel(StudyBuddy.Web.RazorPages.Data.StudiBudiContext context)
+        {
+            _context = context;
+        }
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ Chatter/StudyBuddy.Web.RazorPages/Pages/TeacherPage/TeacherProfile.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StudyBuddy.Web.RazorPages.Data;
+using StudyBuddy.Web.RazorPages.Models;
+
+namespace StudyBuddy.Web.RazorPages.Pages.TeacherPage
+{
+    public class TeacherProfileModel : PageModel
+    {
+        private readonly StudyBuddy.Web.RazorPages.Data.StudiBudiContext _context;
+
+        public TeacherProfileModel(StudyBuddy.Web.RazorPages.Data.StudiBudiContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
