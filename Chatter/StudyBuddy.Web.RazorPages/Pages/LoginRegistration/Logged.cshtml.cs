ffc3d8faa5a741af8b1881668420a32e5229cfdc
Dautartas <dautartasvalvonis@gmail.com>
2019-11-09 00:49:03 +0200

    Login page () With an error

---
+++ Chatter/StudyBuddy.Web.RazorPages/Pages/LoginRegistration/Logged.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudyBuddy.Web.RazorPages.Pages
+{
+    public class LoggedModel : PageModel
+    {
+        public string Username;
+        public void OnGet()
+        {
+           Username = HttpContext.Session.GetString("username");
+        }
+    }
+}
---
+++ Chatter/StudyBuddy.Web.RazorPages/Pages/LoginRegistration/Login.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudyBuddy.Web.RazorPages.Pages
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public string Username { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+        [BindProperty]
+        public string Profession { get; set; }
+
+        public string Msg;
+        public void OnGet()
+        {
+
+        }
+        public IActionResult OnGetLogout()
+        {
+            HttpContext.Session.Remove("username");
+            return Page();
+        }
+        public IActionResult OnPost()
+        {
+            if(!String.IsNullOrEmpty(Username)||!String.IsNullOrEmpty(Password))
+            {
+                if (Username.Equals("aaa") && Password.Equals("aaa"))
+                {
+                    HttpContext.Session.SetString("username", Username);
+                    return RedirectToPage("Logged");
+                }
+                else
+                {
+                    Msg = "Invalid";
+                    return Page();
+                }
+            }
+            else {
+                Msg = "Invalid";
+                return Page();
+            }
+        }
+    }
+}
