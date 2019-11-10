d3bd8c3634ecf555887613d7957e079620ff4669
andrius <andri@DESKTOP-62HQ01O>
2019-11-10 15:05:57 +0200

    profile

--- Chatter/StudyBuddy.Web.RazorPages/Pages/StudentPage/StudentProfile.cshtml.cs
+++ Chatter/StudyBuddy.Web.RazorPages/Pages/StudentPage/StudentProfile.cshtml.cs
@@ -7,21 +7,31 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using StudyBuddy.Web.RazorPages.Data;
 using StudyBuddy.Web.RazorPages.Models;
+using StudyBuddy.Web.RazorPages.Logic;
 
 namespace StudyBuddy.Web.RazorPages.Pages.StudentPage
 {
     public class StudentProfileModel : PageModel
     {
         private readonly StudyBuddy.Web.RazorPages.Data.StudiBudiContext _context;
+        private readonly IUserInfoLoader _userInfoLoader;
 
-        public StudentProfileModel(StudyBuddy.Web.RazorPages.Data.StudiBudiContext context)
+
+        public StudentProfileModel(IUserInfoLoader userInfoLoader)
         {
-            _context = context;
+            _userInfoLoader = userInfoLoader;
         }
 
-        public void OnGet()
-        {
+        public int StudentId { get; private set;  }
+        public string StudentName { get; private set; }
 
+
+
+
+        public async Task OnGet()
+        {
+            StudentId = 4;
+            StudentName = await _userInfoLoader.GetUserNameById(StudentId);
         }
     }
 }
--- Chatter/StudyBuddy.Web.RazorPages/Pages/TeacherPage/TeacherProfile.cshtml.cs
+++ Chatter/StudyBuddy.Web.RazorPages/Pages/TeacherPage/TeacherProfile.cshtml.cs
@@ -7,21 +7,31 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using StudyBuddy.Web.RazorPages.Data;
 using StudyBuddy.Web.RazorPages.Models;
+using StudyBuddy.Web.RazorPages.Logic;
 
 namespace StudyBuddy.Web.RazorPages.Pages.TeacherPage
 {
     public class TeacherProfileModel : PageModel
     {
         private readonly StudyBuddy.Web.RazorPages.Data.StudiBudiContext _context;
+        private readonly IUserInfoLoader _userInfoLoader;
 
-        public TeacherProfileModel(StudyBuddy.Web.RazorPages.Data.StudiBudiContext context)
+        public TeacherProfileModel(IUserInfoLoader userInfoLoader)
         {
-            _context = context;
+            _userInfoLoader = userInfoLoader;
         }
 
-        public void OnGet()
-        {
+        public int TeacherID { get; private set; }
+        public string TeacherName { get; private set; }
 
+
+        public async Task OnGet()
+        {
+            TeacherID = 4;
+            TeacherName = await _userInfoLoader.GetUserNameById(TeacherID);
         }
+        
+
+
     }
 }
